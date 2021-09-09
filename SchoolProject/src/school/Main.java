f7ead001baf178b4842252dbf8577175722842b8
Viren <Viren.Sagpariya@fullstacklabs.ca>
2021-09-09 12:24:15 -0600

    Main file edited

--- SchoolProject/src/school/Main.java
+++ SchoolProject/src/school/Main.java
@@ -2,6 +2,8 @@ package school;
 
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,13 +17,25 @@ public class Main {
         do{
             System.out.println("Enter the name of the student:  (no less than 3 chars and no more than 50 chars)");
             name = scanner.nextLine();
+            if(name.equals("x")|| name.equals("X"))
+            {
+                exit(0);
+            }
+            if (name.length() < 3 || name.length() > 50){
+                System.out.println("Please enter valid name for school");
+            }
         } while(name.length() < 3 || name.length() > 50);
 
         int age;
         do {
             System.out.println("Enter the age of the student: ");
             age = scanner.nextInt();
+            if(age < 4 || age > 17){
+                System.out.println("No schools available, Please enter valid age for school");
+            }
         } while(age < 4 || age > 17);
+
+
 
     }
 }
